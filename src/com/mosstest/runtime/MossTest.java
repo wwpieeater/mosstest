47ac975e8fe7500151d17e361ff7a23c2200f5af
Robert Arkenin <rarkenin@gmail.com>
2013-04-28 16:50:00 -0400

    Initial commit with useful data.

---
+++ src/com/mosstest/runtime/MossTest.java
@@ -0,0 +1,26 @@
+package com.mosstest.runtime;
+import java.io.IOException;
+
+import org.apache.commons.cli.Options;
+
+
+public class MossTest {
+
+	public MossTest() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Options options=new Options();
+		options.addOption("server", false, "Run as standalone server");
+		options.addOption("port", true, "Port number to use");
+		options.addOption("disable-udp", false, "Disable use of UDP");
+		options.addOption("only-udp", false, "Only use UDP");
+		
+		
+	}
+
+}
