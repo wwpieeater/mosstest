a3fe65440ae9fd0376d69a71bc1a494b19977939
rarkenin <rarkenin@gmail.com>
2013-07-27 18:03:56 -0400

    Git broke, I think.

--- src/org/nodetest/servercore/MossEvent.java
+++ src/org/nodetest/servercore/MossEvent.java
@@ -3,7 +3,7 @@ package org.nodetest.servercore;
 public class MossEvent {
 	public MossEvent(EvtType type, Entity actor, long posx, long posy,
 			long posz, MapNode nodeBefore, MapNode nodeAfter,
-			Entity recvEntity, MossFormspec fspec, MossInventoryAction action,
+			Entity recvEntity, MossFormspec fspec, MossInventoryAction action, double damage,
 			String initiatingMessage, ScriptSandboxBorderToken tok) {
 		if (!(tok instanceof ScriptSandboxBorderToken) || tok == null)
 			throw new SecurityException("Attempt to access controlled resources in the script DMZ.");
@@ -16,12 +16,13 @@ public class MossEvent {
 		this.nodeAfter = nodeAfter;
 		this.recvEntity = recvEntity;
 		this.fspec = fspec;
+		this.damage=damage;
 		this.action = action;
 		this.initiatingMessage = initiatingMessage;
 	}
 
 	public enum EvtType {
-		EVT_DIEPLAYER, EVT_DIGNODE, EVT_GENERATE, EVT_JOINPLAYER, EVT_QUITPLAYER, EVT_NEWPLAYER, EVT_PLACENODE, EVT_FSPEC_OPEN, EVT_FSPEC_SUBMIT, EVT_FSPEC_INVACTION, EVT_THREADSTOP, EVT_ENTITY_PUNCH, EVT_ENTITY_DEATH, EVT_CHATMESSAGE, EVT_SHUTDOWN, EVT_CHATCOMMAND, EVT_NODEMOVE
+		EVT_DIEPLAYER, EVT_DIGNODE, EVT_GENERATE, EVT_JOINPLAYER, EVT_QUITPLAYER, EVT_NEWPLAYER, EVT_PLACENODE, EVT_FSPEC_OPEN, EVT_FSPEC_SUBMIT, EVT_FSPEC_INVACTION, EVT_THREADSTOP, EVT_ENTITY_PUNCH, EVT_ENTITY_DEATH, EVT_CHATMESSAGE, EVT_SHUTDOWN, EVT_CHATCOMMAND, EVT_NODEMOVE, EVT_PLAYERDAMAGE
 	}
 
 	MossEvent.EvtType type;
@@ -32,6 +33,7 @@ public class MossEvent {
 	MapNode nodeBefore;
 	MapNode nodeAfter;
 	Entity recvEntity;
+	double damage;
 	MossFormspec fspec;
 	MossInventoryAction action;
 	String initiatingMessage;
---
+++ src/org/nodetest/servercore/MossRenderChunkEvent.java
@@ -0,0 +1,4 @@
+package org.nodetest.servercore;
+
+public class MossRenderChunkEvent extends MossRenderEvent {
+}
