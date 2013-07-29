9320d5dcace6a2e7a4a17a440f5f1c58dfa721df
Aaron Chan <thatnerd2@gmail.com>
2013-07-29 07:46:16 -0400

    - Ditched flyCam, implemented some of its code to retain camera rotation with the mouse. - Matrix transformations are still screwed up.

--- src/org/nodetest/servercore/RenderProcessor.java
+++ src/org/nodetest/servercore/RenderProcessor.java
@@ -6,9 +6,14 @@ import java.util.concurrent.ArrayBlockingQueue;
 import com.jme3.app.Application;
 import com.jme3.app.SimpleApplication;
 import com.jme3.input.KeyInput;
+import com.jme3.input.MouseInput;
 import com.jme3.input.controls.ActionListener;
+import com.jme3.input.controls.AnalogListener;
 import com.jme3.input.controls.KeyTrigger;
+import com.jme3.input.controls.MouseAxisTrigger;
 import com.jme3.material.Material;
+import com.jme3.math.Matrix3f;
+import com.jme3.math.Quaternion;
 import com.jme3.math.Vector3f;
 import com.jme3.scene.Geometry;
 import com.jme3.scene.Node;
@@ -20,6 +25,9 @@ public class RenderProcessor extends SimpleApplication {
 	private Random rand = new Random();
 	private float speed = (float) 0.001;
 	private float[] locChanges = {0,0,0};
+	private boolean invertY = false;
+	private Vector3f initialUpVec;
+	protected float rotationSpeed = 1f;
 	private Node worldNode;
 	public static ArrayBlockingQueue<MossRenderEvent> renderEventQueue = new ArrayBlockingQueue<>(
 			24000, false);
@@ -28,7 +36,7 @@ public class RenderProcessor extends SimpleApplication {
 	public void simpleUpdate (float tpf) {
 		MoveWorld(locChanges[0], locChanges[1], locChanges[2]);
 		
-		
+		inputManager.setCursorVisible(false); 
 		MossRenderEvent myEvent = renderEventQueue.poll();
 		if (myEvent instanceof MossRenderStopEvent) {
 			System.out.println("Thread shutting down");
@@ -47,9 +55,6 @@ public class RenderProcessor extends SimpleApplication {
 		    mat.setColor("Color", ColorRGBA.Blue);
 		    geom.setMaterial(mat);
 		    worldNode.attachChild(geom);
-		}
-		else if (myEvent instanceof MossRenderPlayerMoveEvent) {
-			cam.setLocation(((MossRenderPlayerMoveEvent) myEvent).getPos());
 		}
 			//Add more events
 	}
@@ -72,8 +77,10 @@ public class RenderProcessor extends SimpleApplication {
 		worldNode = new Node("world");
 		rootNode.attachChild(worldNode);
 		renderEventQueue.add(new MossRenderChunkEvent());
-		flyCam.setMoveSpeed(0);
+		flyCam.setEnabled(false);
+		initialUpVec = cam.getUp().clone();
 		initKeyBindings();
+		
 	}
 	
 	private void MoveWorld (float cx, float cy, float cz) {
@@ -82,6 +89,26 @@ public class RenderProcessor extends SimpleApplication {
 				worldNode.getLocalTranslation().y+cy,
 				worldNode.getLocalTranslation().z+cz));
 	}
+	
+	private void rotateCamera(float value, Vector3f axis){
+
+        Matrix3f mat = new Matrix3f();
+        mat.fromAngleNormalAxis(rotationSpeed * value, axis);
+
+        Vector3f up = cam.getUp();
+        Vector3f left = cam.getLeft();
+        Vector3f dir = cam.getDirection();
+
+        mat.mult(up, up);
+        mat.mult(left, left);
+        mat.mult(dir, dir);
+
+        Quaternion q = new Quaternion();
+        q.fromAxes(left, up, dir);
+        q.normalizeLocal();
+
+        cam.setAxes(q);
+    }
 	
 	private ActionListener actionListener = new ActionListener() {
 	    public void onAction(String name, boolean keyPressed, float tpf) {
@@ -99,9 +126,18 @@ public class RenderProcessor extends SimpleApplication {
 	    	
 	    	if (name.equals("Back") && keyPressed) {locChanges[2] = -speed;} //On key down
 	    	else if (name.equals("Back") && !keyPressed && locChanges[2] == -speed) {locChanges[2] = 0;} //on key up
+	    	
+	    	
 	    }
 	};
-	
+	private AnalogListener analogListener = new AnalogListener () {
+		public void onAnalog (String name, float value, float tpf) {
+			if (name.equals("CAM_Left")){rotateCamera(value, initialUpVec);}
+		   	else if (name.equals("CAM_Right")){rotateCamera(-value, initialUpVec);}
+		       else if (name.equals("CAM_Up")){rotateCamera(-value * (invertY ? -1 : 1), cam.getLeft());}
+		   	else if (name.equals("CAM_Down")){rotateCamera(value * (invertY ? -1 : 1), cam.getLeft());}
+		}
+	};
 	private void initKeyBindings () {
 		//inputManager.addMapping("Test", new KeyTrigger(KeyInput.KEY_P));
 		inputManager.addMapping("Left", new KeyTrigger(KeyInput.KEY_A));
@@ -109,10 +145,27 @@ public class RenderProcessor extends SimpleApplication {
 		inputManager.addMapping("Forward", new KeyTrigger(KeyInput.KEY_W));
 		inputManager.addMapping("Back", new KeyTrigger(KeyInput.KEY_S));
 		
+		inputManager.addMapping("CAM_Left", new MouseAxisTrigger(MouseInput.AXIS_X, true),
+                new KeyTrigger(KeyInput.KEY_LEFT));
+
+		inputManager.addMapping("CAM_Right", new MouseAxisTrigger(MouseInput.AXIS_X, false),
+		                 new KeyTrigger(KeyInput.KEY_RIGHT));
+		
+		inputManager.addMapping("CAM_Up", new MouseAxisTrigger(MouseInput.AXIS_Y, false),
+		              new KeyTrigger(KeyInput.KEY_UP));
+		
+		inputManager.addMapping("CAM_Down", new MouseAxisTrigger(MouseInput.AXIS_Y, true),
+		                new KeyTrigger(KeyInput.KEY_DOWN));
+		
 		//inputManager.addListener(actionListener, "Test");
 		inputManager.addListener(actionListener, "Left");
 		inputManager.addListener(actionListener, "Right");
 		inputManager.addListener(actionListener, "Forward");
 		inputManager.addListener(actionListener, "Back");
+		inputManager.addListener(analogListener, "CAM_Left");
+		inputManager.addListener(analogListener, "CAM_Right");
+		inputManager.addListener(analogListener, "CAM_Up");
+		inputManager.addListener(analogListener, "CAM_Down");
+		
 	}
 }
