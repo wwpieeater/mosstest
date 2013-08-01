ff994f267c7d23192d4e2a0fb26b189a5508f865
rarkenin <rarkenin@gmail.com>
2013-08-01 13:29:15 -0400

    Stupid reference logic...

--- src/net/mosstest/servercore/NodeCache.java
+++ src/net/mosstest/servercore/NodeCache.java
@@ -1,54 +1,85 @@
 package net.mosstest.servercore;
 
+import java.lang.ref.Reference;
+import java.lang.ref.ReferenceQueue;
 import java.lang.ref.SoftReference;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicBoolean;
 
-import org.apache.commons.collections4.map.HashedMap;
-import org.apache.commons.collections4.map.LRUMap;
 
-public class NodeCache{
+public class NodeCache {
 
 	private static final long serialVersionUID = 8915103950141113423L;
-
-	
-
-	private static boolean init;
-	private static HashMap<Position, SoftReference<MapChunk>> chunks=new HashMap<>();
-
+	private static HashMap<Position, SoftReference<MapChunk>> chunks = new HashMap<>();
+	private static ReferenceQueue<MapChunk> rqueue = new ReferenceQueue<>();
+	private static AtomicBoolean runFinalizeThread=new AtomicBoolean(true);
+//	public static void init() {
+//		new Thread(new Runnable() {
+//
+//			@Override
+//			public void run() {
+//				while(runFinalizeThread.get()) {
+//					Reference<? extends MapChunk> ref;
+//					try {
+//						ref = rqueue.remove();
+//					
+//					synchronized (chunks) {
+//						synchronized (MapDatabase.class) {
+//							MapDatabase.addMapChunk(ref.get().pos, ref.get());
+//						}
+//					}
+//						ref.clear();
+//					} catch (InterruptedException e) {
+//						// TODO Auto-generated catch block
+//						e.printStackTrace();
+//					}
+//				}
+//				
+//			}
+//			
+//		}, "mapCacheGcHandler").run();
+//	}
 	public static MapChunk getChunk(Position pos) {
 		synchronized (chunks) {
 			synchronized (MapDatabase.class) {
 				MapChunk ourChunk = null;
 				ourChunk = chunks.get(pos).get();
-				if (ourChunk == null)
+				if (ourChunk == null) {
 					ourChunk = MapDatabase.getChunk(pos);
-
+					chunks.put(pos, new SoftReference<MapChunk>(ourChunk,
+							rqueue));
+				}
 				return ourChunk;
 			}
 		}
 	}
-	
+
 	public static MapChunk getChunkClient(Position pos) {
 		synchronized (chunks) {
-			
-				MapChunk ourChunk = null;
-				ourChunk = chunks.get(pos).get();
-				if (ourChunk == null)
-					ClientManager.getNetworkingManager().sendChunkRequest(pos);
 
-				return ourChunk;
-			
+			MapChunk ourChunk = null;
+			ourChunk = chunks.get(pos).get();
+			if (ourChunk == null)
+				ClientManager.getNetworkingManager().sendChunkRequest(pos);
+
+			return ourChunk;
+
 		}
 	}
 
 	public static void setChunkClient(Position pos, MapChunk chunk) {
 		synchronized (chunks) {
 			synchronized (MapDatabase.class) {
-				chunks.put(pos, new SoftReference<MapChunk>(chunk));
+				chunks.put(pos, new SoftReference<MapChunk>(chunk, rqueue));
 			}
 		}
 
+	}
+	
+	
+	public static void shutdown() {
+		runFinalizeThread.set(false);
 	}
 
 	public static void setChunk(Position pos, MapChunk chunk) {
@@ -61,6 +92,4 @@ public class NodeCache{
 
 	}
 
-
-	
 }
