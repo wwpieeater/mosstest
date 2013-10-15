452b7e2c59d8f328e77db9ecc9943af898882ea6
Michael Rosenbloom <river109a@hotmail.com>
2013-10-15 18:54:08 -0400

    JavaApi and related class stubs.

---
+++ src/net/mosstest/sandbox/lang/Runtime.java
@@ -0,0 +1,5 @@
+package net.mosstest.sandbox.lang;
+
+public class Runtime {
+	//todo sandbox
+}
---
+++ src/net/mosstest/sandbox/util/Enumeration.java
@@ -0,0 +1,5 @@
+package net.mosstest.sandbox.util;
+
+public class Enumeration {
+	//todo sandbox
+}
