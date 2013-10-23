248696b079f6d5de0db21195dcd5310e76af2e42
hexafraction <hexafraction@gmail.com>
2013-10-23 13:15:52 -0400

    Another minifix

--- src/net/mosstest/servercore/ItemManager.java
+++ src/net/mosstest/servercore/ItemManager.java
@@ -12,17 +12,18 @@ public class ItemManager {
 	public static MossItem getForNode(MapNode mapNode) {
 		MossItem item = itemsForNode.get(mapNode);
 		if (item == null) {
-			item = createForNode(mapNode);
+			//item = createForNode(mapNode); //FIXME
 			itemsForNode.put(mapNode, item);
 			return item;
 		} else
 			return item;
 	}
 
-	private static MossItem createForNode(MapNode mapNode) {
-		MossItem mi=new MossItem();
+	/*private static MossItem createForNode(MapNode mapNode) {
+		MossItem mi=new MossItem(mapNode.texture, mapNode, 1, true, mapNode.userFacingName, mapNode.nodeName)
 		return mi;
 		//TODO todo
 	}
-
+*/
+	//FIXME above
 }
