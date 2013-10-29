881d94879478e296c615252ee673eaf52699e124
rarkenin <rarkenin@gmail.com>
2013-10-29 15:22:44 -0400

    Create first unit-tests

---
+++ tests/net/mosstest/tests/MainServerTest.java
@@ -0,0 +1,12 @@
+package net.mosstest.tests;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runner.RunWith;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+	MapGeneratorTest.class,
+	})
+public class MainServerTest {
+//empty class
+}
