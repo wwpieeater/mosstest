6a5f9ac84f77e8eadf78e0f56d08d7b97a37031a
Aaron Chan <thatnerd2@gmail.com>
2013-11-15 20:33:51 -0500

    Serious code cleaning, much more elegant now.  Added temporary surface normals that seem to work. Still tests locally.

--- src/net/mosstest/servercore/RenderProcessor.java
+++ src/net/mosstest/servercore/RenderProcessor.java
@@ -17,6 +17,7 @@ import com.jme3.input.controls.AnalogListener;
 import com.jme3.input.controls.KeyTrigger;
 import com.jme3.input.controls.MouseAxisTrigger;
 import com.jme3.light.DirectionalLight;
+import com.jme3.light.PointLight;
 import com.jme3.light.SpotLight;
 import com.jme3.material.Material;
 import com.jme3.math.FastMath;
@@ -40,10 +41,12 @@ import net.mosstest.scripting.Position;
 
 public class RenderProcessor extends SimpleApplication {
 
-	private final float speed = 3f;
-	private final float playerHeight = 25;
-	private final float blockSize = 10f;
-	private final float rotationSpeed = 1f;
+	private final float SPEED = 3f;
+	private final float PLAYER_HEIGHT = 25;
+	private final float BLOCK_SIZE = 20f;
+	private final float ROTATION_SPEED = 1f;
+	private final double BLOCK_OFFSET_FROM_CENTER = 8 * BLOCK_SIZE;
+	private final double CHUNK_OFFSET = 16 * BLOCK_SIZE;
 	private float[] locChanges = { 0, 0, 0 };
 	private double lastTime;
 	private boolean invertY = false;
@@ -51,6 +54,7 @@ public class RenderProcessor extends SimpleApplication {
 	private Vector3f initialUpVec;
 	private Node worldNode;
 	private SpotLight spot = new SpotLight();
+	private PointLight lamp = new PointLight();
 	private DirectionalLight sun = new DirectionalLight();
 	private HashMap<Position, RenderMapChunk> allChunks = new HashMap<Position, RenderMapChunk>();
 
@@ -81,18 +85,24 @@ public class RenderProcessor extends SimpleApplication {
 		lastTime = 0;
 		worldNode = new Node("world");
 		rootNode.attachChild(worldNode);
-		spot.setSpotRange(150f);
+		spot.setSpotRange(300f);
 		spot.setSpotInnerAngle(15f * FastMath.DEG_TO_RAD);
 		spot.setSpotOuterAngle(35f * FastMath.DEG_TO_RAD);
 		spot.setColor(ColorRGBA.White.mult(3f));
 		spot.setPosition(cam.getLocation());
 		spot.setDirection(cam.getDirection());
-
+		//rootNode.addLight(spot);
+		
+		lamp.setColor(ColorRGBA.Yellow);
+		lamp.setRadius(4f);
+		lamp.setPosition(cam.getLocation());
+		//rootNode.addLight(lamp);
+		
 		sun.setColor(ColorRGBA.White);
 		sun.setDirection(new Vector3f(-.5f, -.5f, -.5f).normalizeLocal());
 		rootNode.addLight(sun);
-		rootNode.addLight(spot);
-		testChunkEvents();
+		
+		localChunkTest();
 		//testPeparator();
 		flyCam.setEnabled(false);
 		initialUpVec = cam.getUp().clone();
@@ -114,26 +124,11 @@ public class RenderProcessor extends SimpleApplication {
 		MossRenderEvent myEvent = renderEventQueue.poll();
 		if (myEvent instanceof MossRenderStopEvent) {
 			System.out.println("Thread shutting down");
-		} else if (myEvent instanceof MossRenderChunkEvent) {
+		}
+		else if (myEvent instanceof MossRenderChunkEvent) {
 			renderChunk(((MossRenderChunkEvent) myEvent).getChk(),
 					((MossRenderChunkEvent) myEvent).getPos());
-		}/*
-		 * else if (myEvent instanceof MossNodeAddEvent) { int x =
-		 * ((MossNodeAddEvent) myEvent).getX(); int y = ((MossNodeAddEvent)
-		 * myEvent).getY(); int z = ((MossNodeAddEvent) myEvent).getZ();
-		 * Position pos = ((MossNodeAddEvent) myEvent).getPosition();
-		 * 
-		 * short defRef = ((MossNodeAddEvent) myEvent).getDef(); MapNode def =
-		 * /*NodeManager.getNode(defRef)null; Material mat =
-		 * getMaterial(defRef); allChunks.get(pos).addNode(def, mat, blockSize,
-		 * x, y, z); Vector3f loc = allChunks.get(pos).getNodeLoc(x, y, z,
-		 * blockSize); RenderNode geom = new RenderNode (mat, loc, blockSize,
-		 * def); worldNode.attachChild(geom);
-		 * System.out.println("ADDED A NODE"); } else if (myEvent instanceof
-		 * MossRenderAddAssetPath) { String path = ((MossRenderAddAssetPath)
-		 * myEvent).getPath(); assetManager.registerLocator(path,
-		 * com.jme3.asset.plugins.FileLocator.class); }
-		 */
+		}
 	}
 	
 	public void getChunk (Position pos) {
@@ -145,18 +140,17 @@ public class RenderProcessor extends SimpleApplication {
 	}
 	
 	public void renderChunk(MapChunk chk, Position pos) {
-		double offset = 16 * blockSize;
-		int ixOffset = 0;
-		Mesh bigMesh = new Mesh ();
-		FloatBuffer tcoords = FloatBuffer.allocate(500000);
+		int vertexIndexCounter = 0;
+		
+		Mesh completeMesh = new Mesh ();
 		FloatBuffer vertices = FloatBuffer.allocate(500000);
 		FloatBuffer normals = FloatBuffer.allocate(500000);
 		IntBuffer indices = IntBuffer.allocate(500000);
 		//RenderNode[][][] nodesInChunk = new RenderNode[16][16][16];
 
-		for (byte i = 0; i < 5; i++) {
-			for (byte j = 0; j < 5; j++) {
-				for (byte k = 0; k < 5; k++) {
+		for (byte i = 0; i < 16; i++) {
+			for (byte j = 0; j < 16; j++) {
+				for (byte k = 0; k < 16; k++) {
 					int nVal = chk.getNodeId(i, j, k);
 					//MapNode node = nManager.getNode((short) nVal);
 					//Material mat = getMaterial((short) nVal);
@@ -164,62 +158,64 @@ public class RenderProcessor extends SimpleApplication {
 					
 					else {
 						
-						float x = (float) ((pos.x + (32 * blockSize * pos.x)) - offset + (i * 3 * blockSize));
-						float y = (float) ((pos.y - playerHeight) - (j * 3 * blockSize));
-						float z = (float) ((pos.z + (32 * blockSize * pos.z)) - offset  + (k * 3 * blockSize));
-						
-						vertices.put(x).put(y).put(z); //Front face
-						vertices.put(x).put(y - 2*blockSize).put(z);
-						vertices.put(x + 2*blockSize).put(y).put(z);
-						vertices.put(x + 2*blockSize).put(y - 2*blockSize).put(z); //Top Face
-						vertices.put(x).put(y).put(z + 2*blockSize);
-						vertices.put(x + 2*blockSize).put(y).put(z + 2*blockSize);
-						vertices.put(x + 2*blockSize).put(y - 2*blockSize).put(z + 2*blockSize); //right face
-						vertices.put(x).put(y - 2*blockSize).put(z + 2*blockSize); //left face
+						float x = (float) ((pos.x + (CHUNK_OFFSET * pos.x)) - BLOCK_OFFSET_FROM_CENTER + (i * BLOCK_SIZE));
+						float y = (float) ((pos.y - PLAYER_HEIGHT) - (j * BLOCK_SIZE));
+						float z = (float) ((pos.z + (CHUNK_OFFSET * pos.z)) - BLOCK_OFFSET_FROM_CENTER  + (k * BLOCK_SIZE));
 
-						indices.put(ixOffset + 3).put(ixOffset + 1).put(ixOffset + 0);//front
-						indices.put(ixOffset + 3).put(ixOffset + 0).put(ixOffset + 2);
-						indices.put(ixOffset + 4).put(ixOffset + 2).put(ixOffset + 0);//top
-						indices.put(ixOffset + 4).put(ixOffset + 5).put(ixOffset + 2);
-						indices.put(ixOffset + 3).put(ixOffset + 2).put(ixOffset + 6);//right
-						indices.put(ixOffset + 2).put(ixOffset + 5).put(ixOffset + 6);
-						indices.put(ixOffset + 0).put(ixOffset + 1).put(ixOffset + 7);//left
-						indices.put(ixOffset + 0).put(ixOffset + 7).put(ixOffset + 4);
-						indices.put(ixOffset + 4).put(ixOffset + 6).put(ixOffset + 5);//back
-						indices.put(ixOffset + 4).put(ixOffset + 7).put(ixOffset + 6);
-						indices.put(ixOffset + 1).put(ixOffset + 6).put(ixOffset + 7);//bottom
-						indices.put(ixOffset + 1).put(ixOffset + 3).put(ixOffset + 6);
+						vertices.put(x).put(y).put(z); //Front face
+						vertices.put(x).put(y - BLOCK_SIZE).put(z);
+						vertices.put(x + BLOCK_SIZE).put(y).put(z);
+						vertices.put(x + BLOCK_SIZE).put(y - BLOCK_SIZE).put(z); //Top Face
+						vertices.put(x).put(y).put(z + BLOCK_SIZE);
+						vertices.put(x + BLOCK_SIZE).put(y).put(z + BLOCK_SIZE);
+						vertices.put(x + BLOCK_SIZE).put(y - BLOCK_SIZE).put(z + BLOCK_SIZE); //right face
+						vertices.put(x).put(y - BLOCK_SIZE).put(z + BLOCK_SIZE); //left face
+						
+						System.out.println("V1: "+x+","+y+","+z);
+						System.out.println("V2: "+x+","+(y-BLOCK_SIZE)+","+z);
+						System.out.println("V3: "+(x+BLOCK_SIZE)+","+y+","+z);
+						
+						for(int m=0; m<8; m++) {
+							normals.put(0).put(0).put(10);
+						}
+						
+						indices.put(vertexIndexCounter + 3).put(vertexIndexCounter + 1).put(vertexIndexCounter + 0);//front
+						indices.put(vertexIndexCounter + 3).put(vertexIndexCounter + 0).put(vertexIndexCounter + 2);
+						indices.put(vertexIndexCounter + 4).put(vertexIndexCounter + 2).put(vertexIndexCounter + 0);//top
+						indices.put(vertexIndexCounter + 4).put(vertexIndexCounter + 5).put(vertexIndexCounter + 2);
+						indices.put(vertexIndexCounter + 3).put(vertexIndexCounter + 2).put(vertexIndexCounter + 6);//right
+						indices.put(vertexIndexCounter + 2).put(vertexIndexCounter + 5).put(vertexIndexCounter + 6);
+						indices.put(vertexIndexCounter + 0).put(vertexIndexCounter + 1).put(vertexIndexCounter + 7);//left
+						indices.put(vertexIndexCounter + 0).put(vertexIndexCounter + 7).put(vertexIndexCounter + 4);
+						indices.put(vertexIndexCounter + 4).put(vertexIndexCounter + 6).put(vertexIndexCounter + 5);//back
+						indices.put(vertexIndexCounter + 4).put(vertexIndexCounter + 7).put(vertexIndexCounter + 6);
+						indices.put(vertexIndexCounter + 1).put(vertexIndexCounter + 6).put(vertexIndexCounter + 7);//bottom
+						indices.put(vertexIndexCounter + 1).put(vertexIndexCounter + 3).put(vertexIndexCounter + 6);
 						
 						
-						//RenderNode geom = new RenderNode(mat, loc, blockSize, NodeManager.getNode((short)nVal)null);
+						
+						//RenderNode geom = new RenderNode(mat, loc, BLOCK_SIZE, NodeManager.getNode((short)nVal)null);
 						//nodesInChunk[i][j][k] = geom;
-						ixOffset += 8;
-						System.out.println(ixOffset);
+						vertexIndexCounter += 8;
 					}
 
 				}
 			}
 		}
 		Material mat = getMaterial((short) 1);
-		bigMesh.setBuffer(Type.Position, 3, vertices);
-		bigMesh.setBuffer(Type.Index, 3, indices);
-		bigMesh.updateBound();
-		Geometry geom = new Geometry("chunkMesh", bigMesh);
+		completeMesh.setBuffer(Type.Position, 3, vertices);
+		completeMesh.setBuffer(Type.Normal, 3, normals);
+		completeMesh.setBuffer(Type.Index, 3, indices);
+		completeMesh.updateBound();
+		Geometry geom = new Geometry("chunkMesh", completeMesh);
 		geom.setMaterial(mat);
 		worldNode.attachChild(geom);
 		/*RenderMapChunk thisChunk = new RenderMapChunk(nodesInChunk, x, y, z);
 		allChunks.put(pos, thisChunk);*/
 	}
-	private void addVertex (float x, float y, float z, FloatBuffer buffer) {
-		buffer.put(x);
-		buffer.put(y);
-		buffer.put(z);
-	}
 	
-	private void addTriangleIndex (int a, int b, int c, IntBuffer buffer) {
-		buffer.put(a);
-		buffer.put(b);
-		buffer.put(c);
+	private void calculateAndStoreSurfaceNormal (float x, float y, float z, FloatBuffer normals) {
+		
 	}
 	
 	private void testPreparator() {
@@ -240,39 +236,30 @@ public class RenderProcessor extends SimpleApplication {
 		// getChunk(p7);
 	}
 
-	private void testChunkEvents() {
-		Position pos = new Position(0, 0, 0, 0);
-		Position pos2 = new Position(1, 0, 1, 0);
-		boolean[][][] testModified = new boolean[16][16][16];
-		boolean[][][] tM2 = new boolean[16][16][16];
-		for (boolean[][] l1 : testModified) {
-			for (boolean[] l2 : l1) {
-				Arrays.fill(l2, false);
+	private void localChunkTest() {
+		for(int i=0; i<2; i++) {
+			for(int j=0; j<1; j++) {
+				for(int k=0; k<1; k++) {
+					Position pos = new Position(i, j, k, 0);
+					boolean[][][] modifiedNodes = new boolean[16][16][16];
+					for(boolean[][] m : modifiedNodes) {
+						for(boolean[] n : m) {
+							Arrays.fill(n, false);
+						}
+					}
+					
+					int[][][] nodeIds = new int[16][16][16];
+					for(int[][] m : nodeIds) {
+						for (int[] n : m) {
+							Arrays.fill(n, 1);
+						}
+					}
+					
+					MapChunk chunk = new MapChunk (pos, nodeIds, modifiedNodes);
+					renderChunk(chunk, pos);
+				}
 			}
 		}
-		for (boolean[][] l1 : tM2) {
-			for (boolean[] l2 : l1) {
-				Arrays.fill(l2, false);
-			}
-		}
-
-		int[][][] tN2 = new int[16][16][16];
-		int[][][] testNodes = new int[16][16][16];
-		for (int[][] l1 : testNodes) {
-			for (int[] l2 : l1) {
-				Arrays.fill(l2, 1);
-			}
-		}
-		for (int[][] l1 : tN2) {
-			for (int[] l2 : l1) {
-				Arrays.fill(l2, 1);
-			}
-		}
-
-		MapChunk ch = new MapChunk(pos, testNodes, testModified);
-		//MapChunk ch2 = new MapChunk(pos2, tN2, tM2);
-		renderChunk(ch, pos);
-		//renderChunk(ch2, pos2);
 		GeometryBatchFactory.optimize(worldNode);
 	}
 	 
@@ -280,13 +267,15 @@ public class RenderProcessor extends SimpleApplication {
 		Material mat = null;
 		switch (nVal) {
 		case 1:
-			/*mat = new Material(assetManager,
-					"Common/MatDefs/Light/Lighting.j3md");*/
-			mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+			mat = new Material(assetManager,
+					"Common/MatDefs/Light/Lighting.j3md");
+			mat.setBoolean("UseMaterialColors", true);
+			mat.setColor("Ambient", ColorRGBA.Green);
+			mat.setColor("Diffuse", ColorRGBA.Green);
+			
+			/*mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
 			mat.setColor("Color", ColorRGBA.Green);
-			//mat.setBoolean("UseMaterialColors", true);
-			//mat.setColor("Ambient", ColorRGBA.Green);
-			//mat.setColor("Diffuse", ColorRGBA.Green);
+			*/
 		}
 		return mat;
 	}
@@ -304,13 +293,10 @@ public class RenderProcessor extends SimpleApplication {
 				.addLocal(0f, -cy, 0f));
 	}
 
-	/**
-	 * Runs when the mouse moves to look around.
-	 */
 	private void rotateCamera(float value, Vector3f axis) {
 
 		Matrix3f mat = new Matrix3f();
-		mat.fromAngleNormalAxis(rotationSpeed * value, axis);
+		mat.fromAngleNormalAxis(ROTATION_SPEED * value, axis);
 
 		Vector3f up = cam.getUp();
 		Vector3f left = cam.getLeft();
@@ -325,13 +311,8 @@ public class RenderProcessor extends SimpleApplication {
 		q.normalizeLocal();
 
 		cam.setAxes(q);
-
-		spot.setDirection(cam.getDirection());
 	}
 
-	/**
-	 * Set up key bindings and event listeners for key bindings
-	 */
 	private void initKeyBindings() {
 		inputManager.addMapping("Jump", new KeyTrigger(KeyInput.KEY_SPACE));
 		inputManager.addMapping("Down", new KeyTrigger(KeyInput.KEY_LSHIFT));
@@ -380,7 +361,7 @@ public class RenderProcessor extends SimpleApplication {
 	};
 	private ActionListener actionListener = new ActionListener() {
 		public void onAction(String name, boolean keyPressed, float tpf) {
-			if (name.equals("Jump") && keyPressed/* && jumpSpeed == 0 */) {
+			if (name.equals("Jump") && keyPressed/* && jumpSPEED == 0 */) {
 				locChanges[1] = 2f;
 			} else if (name.equals("Jump") && !keyPressed) {
 				locChanges[1] = 0f;
@@ -393,30 +374,30 @@ public class RenderProcessor extends SimpleApplication {
 			}
 
 			if (name.equals("Left") && keyPressed) {
-				locChanges[0] = speed;
+				locChanges[0] = SPEED;
 			} else if (name.equals("Left") && !keyPressed
-					&& locChanges[0] == speed) {
+					&& locChanges[0] == SPEED) {
 				locChanges[0] = 0;
 			}
 
 			if (name.equals("Right") && keyPressed) {
-				locChanges[0] = -speed;
+				locChanges[0] = -SPEED;
 			} else if (name.equals("Right") && !keyPressed
-					&& locChanges[0] == -speed) {
+					&& locChanges[0] == -SPEED) {
 				locChanges[0] = 0;
 			}
 
 			if (name.equals("Forward") && keyPressed) {
-				locChanges[2] = speed;
+				locChanges[2] = SPEED;
 			} else if (name.equals("Forward") && !keyPressed
-					&& locChanges[2] == speed) {
+					&& locChanges[2] == SPEED) {
 				locChanges[2] = 0;
 			}
 
 			if (name.equals("Back") && keyPressed) {
-				locChanges[2] = -speed;
+				locChanges[2] = -SPEED;
 			} else if (name.equals("Back") && !keyPressed
-					&& locChanges[2] == -speed) {
+					&& locChanges[2] == -SPEED) {
 				locChanges[2] = 0;
 			}
 		}
